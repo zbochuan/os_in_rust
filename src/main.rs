e3709d0632b1bb049d1f3ddafdf4c7bfa75cac74
Cheong Zhang <cheong1024@foxmail.com>
2025-12-10 23:19:29 +1100

    vga_buffer implemented

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #![no_std] // do not link std lib
 #![no_main] // disable all rust level entry point
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-#[unsafe(no_mangle)] // do not mangle the name of the function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    loop {}
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)] // do not mangle the name of the function
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
     loop {}
 }
